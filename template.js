2e370b855b301815bbf83d3881fa41db06b453e2
ogarich89 <ogarich89@gmail.com>
2025-06-14 15:57:03 +0200

    Initial commit

---
+++ template.js
@@ -0,0 +1,36 @@
+const _readline = require('readline');
+
+const _reader = _readline.createInterface({
+    input: process.stdin
+});
+
+const _inputLines = [];
+let _curLine = 0;
+
+_reader.on('line', line => {
+    _inputLines.push(line);
+});
+
+const readInt = () => {
+    const number = Number(_inputLines[_curLine]);
+    _curLine++;
+    return number;
+}
+
+const readString = () => {
+    const string = _inputLines[_curLine].trim();
+    _curLine++;
+    return string;
+}
+
+const readArray = () => {
+    const arr = _inputLines[_curLine].trim().split(" ");
+    _curLine++;
+    return arr;
+}
+
+const solve = () => {
+
+}
+
+process.stdin.on('end', solve);
